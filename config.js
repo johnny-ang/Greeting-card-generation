754f227cfe25587e8585317480f26991de562524
johnny-ang <johnny690713@gmail.com>
2026-07-28 12:55:30 +0800

    更新 config.js（管理者工具）

--- config.js
+++ config.js
@@ -171,8 +171,8 @@ const TEMPLATES = {
     },
 
     branch: {
-      x: 0.0244,
-      y: 0.9227,
+      x: 0.1927,
+      y: 0.9194,
       size: 0.04,
       weight: "700",
       color: "#1A3A6B",
