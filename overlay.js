dad8b5ff02bbf078623d44cbc25e037efa4d6ef3
johnny-ang <johnny690713@gmail.com>
2026-09-18 14:23:37 +0800

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -269,10 +269,14 @@ function initTool() {
 
         // 比例座標換算
         function px(cfg) {
-          if (!cfg) return null;
-          return {
-            x:      cfg.x      * W,
-            y:      cfg.y      * H,
+  if (!cfg) return null;
+  const h = cfg.height != null ? cfg.height * H : null;
+  const y = cfg.alignBottom
+    ? H - h
+    : (cfg.y != null ? cfg.y * H : 0);
+  return {
+    x:      cfg.x      * W,
+    y:      y,
             width:  cfg.width  != null ? cfg.width  * W : null,
             height: cfg.height != null ? cfg.height * H : null,
             fit:    cfg.fit,
